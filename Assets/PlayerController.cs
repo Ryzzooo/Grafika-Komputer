230a286aae82e7cd71a72e93806250e4a43bfa64
Ryzzooo <sisaderay.888@gmail.com>
2025-11-10 20:13:05 +0700

    commit

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,49 +2,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // T = T + (1, 0, 0) * speed
+    Vector3 movement;
+    Rigidbody rb;
+    [SerializeField] private float speed = 50f;
 
-    Vector3 kanan = new Vector3(1f, 0f, 0f);
-    Vector3 kiri = new Vector3(-1f, 0f, 0f);
-    Vector3 depan = new Vector3(0f, 0f, 1f);
-    Vector3 belakang = new Vector3(0f, 0f, -1f);
-    Vector3 atas = new Vector3(0f, 1f, 0f);
-    Vector3 bawah = new Vector3(0f, -1f, 0f);
-
-    float speed = 5f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += kanan * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += kiri * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += depan * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += belakang * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.J))
-        {
-            transform.position += atas * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.K))
-        {
-            transform.position += bawah * speed * Time.deltaTime;
-        }
+        Walk();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movement * speed, ForceMode.Acceleration);
+    }
+
+    void Walk()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
     }
 }
